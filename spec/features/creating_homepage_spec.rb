e933a500948c5ffce264dcc3c7f4a6be8a5f5976
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2017-12-17 16:36:39 +0000

    adding a controller and feature spec and wiring them up

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "On visiting the home page" do
+  scenario "the user will see certain content" do
+    visit "/"
+    expect(page).to have_content("Workout Lounge!")
+    expect(page).to have_content("Show off your workout")
+    expect(page).to have_link("Home")
+    expect(page).to have_link("Athletes Den")
+  end
+end
