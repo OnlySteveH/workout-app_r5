b4f96498770f1c46382540d446f411ca796a0bb9
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2017-12-17 16:58:49 +0000

    adding content to pass the feature test

---
+++ app/controllers/dashboards_controller.rb
@@ -0,0 +1,10 @@
+class DashboardsController < ApplicationController
+  
+  def show
+  end
+  
+  def index
+    
+  end
+  
+end
--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "On visiting the home page" do
   scenario "the user will see certain content" do
     visit "/"
-    expect(page).to have_content("Workout Lounge!")
-    expect(page).to have_content("Show off your workout")
     expect(page).to have_link("Home")
     expect(page).to have_link("Athletes Den")
+    expect(page).to have_content("Workout Lounge!")
+    expect(page).to have_content("Show off your workout")
   end
 end
