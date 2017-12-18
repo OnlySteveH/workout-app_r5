53edb840c2895d5737de6f204dba21628c7bf4a4
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2017-12-18 16:47:19 +0000

    getting user feature test to pass with amended layout

---
+++ spec/features/users/signup_user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.feature "On visiting the signup page" do
+  scenario "the user will enter their information" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "steve@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    expect(page).to have_content "You have signed up successfully"
+  end
+end
