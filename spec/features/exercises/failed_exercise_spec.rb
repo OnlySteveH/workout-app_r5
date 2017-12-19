b8194e2f6a593eeba4487be94fa114f13a5e0a77
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2017-12-19 15:32:43 +0000

    amending navbar to use full_name method

---
+++ spec/features/exercises/failed_exercise_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "On creating an exercise" do
+  before do
+    @steve = User.create!(first_name: "Steve", last_name: "Hunter", email: "steve@example.com", password: "password")
+    login_as @steve 
+  end
+  
+  scenario "with valid inputs creates an exercise" do
+    visit root_path
+    click_link "My Lounge"
+    click_link "New workout"
+    expect(page).to have_link "Back"
+    
+    fill_in "Duration", with: "70"
+    fill_in "Workout details", with: ""
+    fill_in "Activity date", with: ""
+    click_button "Create Exercise"
+    
+    expect(page).to have_content("Exercise has not been created")
+    expect(page).not_to have_content("Exercise has been created")
+    expect(page).to have_content("Activity date is required")
+    expect(page).to have_content("Workout details is required")
+    expect(page).to have_content("Duration should be a number")
+    
+    end
+end
